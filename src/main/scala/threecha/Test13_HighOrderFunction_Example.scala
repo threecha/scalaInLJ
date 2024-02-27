d127752b56c4a31474cc15b26cf9ef5cb550380e
yy <yang.yan@quantisantech.com>
2024-02-27 14:49:50 +0800

    feat[high order function] the anonymous simplify

--- src/main/scala/threecha/Test13_HighOrderFunction_Example.scala
+++ src/main/scala/threecha/Test13_HighOrderFunction_Example.scala
@@ -9,14 +9,15 @@ object Test13_HighOrderFunction_Example {
      * Example: 实现集合的+1操作
      *
      */
-    val targetArrays = Array(12,14,29,31,100)
+    val targetArrays = Array(12, 14, 29, 31, 100)
 
-    def arrayOperation(input:Array[Int],op:Int=>Int):Array[Int]= for(element <- input)yield op(element)
-    def addOne(element:Int):Int = element+1
+    def arrayOperation(input: Array[Int], op: Int => Int): Array[Int] = for (element <- input) yield op(element)
 
-    println(arrayOperation(targetArrays,addOne).mkString(","))
-    println(arrayOperation(targetArrays,_+1).mkString(","))
-    println(arrayOperation(targetArrays,a => a+1 ).mkString(","))
+    def addOne(element: Int): Int = element + 1
+
+    println(arrayOperation(targetArrays, addOne).mkString(","))
+    println(arrayOperation(targetArrays, _ + 1).mkString(","))
+    println(arrayOperation(targetArrays, a => a + 1).mkString(","))
 
     /**
      * example2: 定义一个匿名函数，并把它作为赋值给变量fun 函数有三个参数，类型分别为int String Char 返回值类型为Boolean
@@ -24,9 +25,9 @@ object Test13_HighOrderFunction_Example {
      * 要求：调用函数fun(0,"",'0') 得到为false 其他情况得到的值为True
      *
      */
-    val fun = (a:Int,b:String,c:Char) => if(a==0 && b=="" && c =='0') false else true
-    println(fun(1,"",'1'))
-    println(fun(0,"",'0'))
+    val fun = (a: Int, b: String, c: Char) => if (a == 0 && b == "" && c == '0') false else true
+    println(fun(1, "", '1'))
+    println(fun(0, "", '0'))
 
     /**
      * example3: 定义一个函数func 他接受一个Int参数，返回值记作f1 f1接收一个String类型参数 返回函数f2 f2接受Char类型参数 返回一个Boolean值
@@ -35,17 +36,54 @@ object Test13_HighOrderFunction_Example {
      *
      */
 
-   def func(a:Int):String => Char => Boolean =
-     def f1(b:String):Char => Boolean = {
-       def f2(c:Char):Boolean ={
-         if (a == 0 && b == "" && c == '0') false else true
-       }
-       f2
-     }
-     f1
+    def func(a: Int): String => Char => Boolean =
+      def f1(b: String): Char => Boolean = {
+        def f2(c: Char): Boolean = {
+          if (a == 0 && b == "" && c == '0') false else true
+        }
+
+        f2
+      }
+
+      f1
 
     // 以上就是一个套娃过程
     println(func(1)("")('0'))
     println(func(0)("")('0'))
+
+
+    /**
+     * 匿名函数最简原则
+     *
+     * 根据最简原则简化 :类型可以省略 因为可以推导出来
+     */
+
+    def func_v2(a: Int) =
+      def f1(b: String) = {
+        def f2(c: Char) = if (a == 0 && b == "" && c == '0') false else true
+
+        f2
+      }
+
+      f1
+
+    println(func_v2(1)("")('0'))
+    println(func_v2(0)("")('0'))
+
+    def func_v3(a: Int) = (b: String) => (c: Char) => if (a == 0 && b == "" && c == '0') false else true
+
+    def func_v4(a: Int): String => Char => Boolean = b => c => if (a == 0 && b == "" && c == '0') false else true
+
+    /**
+     * v5 : 无法使用 因为无法完成正确的类型推倒
+     */
+    // def func_v5(a: Int) = b => c => if (a == 0 && b == "" && c == '0') false else true
+
+    println(func_v3(1)("")('0'))
+    println(func_v3(0)("")('0'))
+
+    println(func_v4(1)("")('0'))
+    println(func_v4(0)("")('0'))
+
   }
 }
